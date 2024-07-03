ca84a38c4ecef9fad404e39d287a57880c4ca20a
jinho <zazz0907@naver.com>
2024-07-03 11:18:56 +0900

    TDD Cycle 5 refactor (generallization) is done.

--- UnitTestProject/PrimeFactor.cpp
+++ UnitTestProject/PrimeFactor.cpp
@@ -7,20 +7,28 @@ public:
 	vector<int> of(int factor)
 	{
 		vector<int> ret = {};
+		int divide = 0;
 		if (factor > 1)
 		{
 			if (factor == 4)
 			{
-				while (factor % 2 == 0)
+				divide = 2;
+				while (factor % divide == 0)
 				{
-					ret.push_back(2);
+					ret.push_back(divide);
 					factor >>= 1;
 				}
 			}
 			else if (factor == 6)
 			{
-				ret.push_back(2);
-				ret.push_back(3);
+				for (divide = 2; factor > 1; divide++)
+				{
+					while (factor % divide == 0)
+					{
+						ret.push_back(divide);
+						factor /= divide;
+					}
+				}
 			}
 			else
 			{
